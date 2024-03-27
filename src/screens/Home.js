dd4e424ab056adcdda148e94c9d71a004777b224
Atul kumar <atulkashyap1907@gmail.com>
2024-03-27 21:09:51 +0530

    Added Navbar

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbars from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+      <div><Navbars/></div>
+      <div>body</div>
+      <div><Footer/></div>
+    </div>
+  );
+};
